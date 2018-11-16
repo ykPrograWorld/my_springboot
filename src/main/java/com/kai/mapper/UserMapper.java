3aba86b0f9992aecacedc0232a2d2c882c97d856
yekai <boy_kai@126.com>
2018-11-16 22:58:58 +0800

    问题待解决

--- src/main/java/com/kai/mapper/UserMapper.java
+++ src/main/java/com/kai/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.kai.mapper;
 
 import com.kai.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,7 +9,7 @@ import org.apache.ibatis.annotations.Select;
  * @Author: kai
  * @Date: Created in 下午 10:14 2018/11/16
  */
-
+@Mapper
 public interface UserMapper {
 
     @Select("select id,email,nick_name,reg_time,username from user where id = #{id}}")

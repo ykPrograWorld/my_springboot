f7843aab94524b29ffaf4bac99c5985ef3cedcc9
yekai <boy_kai@126.com>
2018-11-16 20:33:14 +0800

    增加热部署配置，idea中需要先修改自身设置才会生效

--- src/main/java/com/kai/controller/PageController.java
+++ src/main/java/com/kai/controller/PageController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: kai
  * @Date: Created in 下午 06:12 2018/11/16
@@ -13,6 +16,13 @@ public class PageController {
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("name","friend ");
+        List<String> stringList= new ArrayList<>();
+        stringList.add("test0");
+        stringList.add("test1");
+        stringList.add("test2");
+        stringList.add("test3");
+
+        model.addAttribute("list",stringList);
         return "index";
     }
 }
